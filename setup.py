349afd551f8e8b0359e944e44d35c7405601c49b
ChrisSkorka <chris.skorka@gmail.com>
2025-10-13 21:15:21 +1100

    fix setup not working on fresh system

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+root_package = 'sysconf'
+
 setup(
-    name='sysconf',
+    name=root_package,
     version='0.1',
-    packages=find_packages('sysconf'),
+    packages=[root_package] + [
+        f"{root_package}.{item}" 
+        for item in find_packages(where=root_package)
+    ],
 )
